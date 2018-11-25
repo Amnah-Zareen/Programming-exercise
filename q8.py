95b04b34377a495b7f5f5c4c7e46e1c247a01676
Amnah-Zareen <45007017+Amnah-Zareen@users.noreply.github.com>
2018-11-25 08:51:07 -0800

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,9 @@
+print("Q8")
+
+username = input("Enter your name :")
+def reverse(username):
+    username = username[::-1]
+    rev_name = reversed(username)
+    print("Your name in reverse order is:", username)
+reverse(username)
+    
