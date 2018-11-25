f3eb1f7889af815ebaa8f87eddf33b6f55329b05
Amnah-Zareen <45007017+Amnah-Zareen@users.noreply.github.com>
2018-11-25 07:17:09 -0800

    Add files via upload

--- q6.py
+++ q6.py
@@ -1,7 +1,51 @@
 print("Q6")
+import math
+print("FIRST EQUATION OF MOTION")
+vi = int(input("Enter the initial velocity :"))
+a = int(input("Enter the acceleration :"))
+t = int(input("Enter the time :"))
+def lawsofmotion (vi,a,t):
+    print("The initial velocity is:" , vi)
+    print("The acceleration is:" , a)
+    print("The time is:" , t)
+    vf = vi + a*t
+    print("The  final velocity is:" , vf)
+lawsofmotion(vi,a,t)
 
-f_law = eval(input("Enter the first law of motion :"))
-def lawsofmotion (f_law):
-    print("The first law of motion is ", f_law)
+print("SECOND EQUATION OF MOTION")
+vi = int(input("Enter the initial velocity :"))
+t = int(input("Enter the time :"))
+a = int(input("Enter the acceleration :"))
+def lawsofmotion(vi,t,a):
+    print("The initial velocity is:" , vi)
+    print("The time is:" , t)
+    print("The acceleration is:" , a)
+    s = (vi)*t + (1/2)*a*(t**2)
+    print("The distance is :" , s)
+lawsofmotion(vi,t,a)
 
-FORCE = MASS * ACCELERATION
+print("THIRD EQUATION OF MOTION")
+vi = int(input("Enter the initial velocity :"))
+a = int(input("Enter the acceleration :"))
+s = int(input("Enter the distance :"))
+def lawsofmotion(vi,a,s):
+     print("The initial velocity is:" , vi)
+     print("The acceleration is:" , a)
+     print("The distance is :" , s)
+     vf = math.sqrt(vi + a*t)
+     print("The final velocity is :" , vf**2)
+lawsofmotion(vi,a,s)
+     
+     
+     
+    
+
+    
+    
+
+
+
+
+
+
+
